6ccb4db214885b15dd5579d0049e13459bcdb4e7
siddamvinay2001 <siddamvinay1368@gmail.com>
2023-06-02 13:59:52 +0530

    Project structuring

---
+++ hospital-webpage/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Webux from './web-ux/web-ux'
+
+function App() {
+
+  return (
+    <>
+      <Webux/>
+    </>
+  )
+}
+
+export default App
---
+++ hospital-webpage/src/web-ux/web-ux.jsx
@@ -0,0 +1,8 @@
+export default function Webux (){
+    return (
+        <div>
+            Configuring page related query params
+        </div>
+    )
+}
+
